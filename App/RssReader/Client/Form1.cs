8182bb58e0a4accd9aed7dba2229af3700975528
Arthur Veys <contact@veys-arthur.fr>
2013-02-20 13:28:18 +0100

    Lancement du projet c#
    creation du projet sur VS 2010

---
+++ App/RssReader/Client/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != null)
+                connect(textBox1.Text);
+            else
+                MessageBox.Show("Veuillez entrer une adresse");
+        }
+
+        private void connect(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
