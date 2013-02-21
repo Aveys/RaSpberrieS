03604f751c8fea22e491869e38d6eae12a13f3a4
Nathanaël COURET <nathanael.couret@etu.univ-savoie.fr>
2013-02-21 17:37:10 +0100

    lib in progress

--- App/RssReader/RssReader/Article.cs
+++ App/RssReader/RssReader/Article.cs
@@ -3,9 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Collections.ObjectModel;
+
 namespace RssReader
 {
-    class Article
+    /**
+     * <summary>
+     * Contains information about a single RSS post.
+     * </summary>
+     * 
+     */
+    class ArticleItem
     {
+        private string m_title;
+        public string title
+        {
+            get
+            {
+                return this.m_title;
+            }
+            set;
+        }
+
+        private string m_author;
+        public string author
+        { 
+            get
+            {
+                return this.m_author;
+            }
+            set;
+        }
+        //content?
+
+        private UInt32 m_id;
+        public UInt32 id
+        {
+            get
+            {
+                return this.m_id;
+            } 
+            set;
+        }
+        private DateTime m_date_pub;
+        public DateTime datePub
+        { 
+            get
+            {
+                return this.m_date_pub;
+            }
+            set;
+        }
+        private Uri m_link;
+        public Uri link
+        { 
+            get
+            {
+                return this.m_link;
+            }
+            set;
+        }
+       
+
+    }
+    /**
+     * <summary>
+     *  
+     * </summary>
+     */
+    class ArticlesData
+    {
+        private ObservableCollection<ArticleItem> m_items = new ObservableCollection<ArticleItem>();
+        public ObservableCollection<ArticleItem> items
+        {
+            get
+            {
+                return this.m_items;
+            }
+            set;
+        }
+        
+        public void add( ArticleItem new_item )
+        {
+            this.items.Add(new_item);
+        }
+
     }
 }
